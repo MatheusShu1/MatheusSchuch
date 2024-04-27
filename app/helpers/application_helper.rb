8f7f0c2093f0404eb20a173abdb4cd42d3cb2da5
Matheus Schuch <102114294+MatheusShu1@users.noreply.github.com>
2024-04-26 21:48:39 -0300

    .
    .

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+    def person_active(person)
+        if person.active?
+            content_tag(:span, class: "text-success") do
+                content_tag(:i, '', class: "bi bi-check-circle")
+            end
+        else
+            content_tag(:span, class: "text-danger") do
+                content_tag(:i, '', class: "bi bi-x-circle")
+            end
+        end
+    end
+
+    def notice_present(notice)
+        if notice.present?
+            content_tag(:div, notice, class: "alert alert-success")
+        end
+    end
 end
